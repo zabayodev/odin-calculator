3b3109addfdd24921a82b66e5105ec588b07d4eb
Emmanuel <zabayo92@gmail.com>
2023-08-02 11:27:56 +0200

    Declaring all the variables and initializing empty variables on the calculator

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hi")
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+document.addEventListener("DOMContentLoaded", function(){
+    //Store all the htm components for usage in js
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+})
