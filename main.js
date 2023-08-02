16387c7e8c788f10f6f7a0fc1dd058969d6c0b66
Emmanuel <zabayo92@gmail.com>
2023-08-02 11:46:20 +0200

    Defining the digits a user can enter on the calculator for digits control

--- main.js
+++ main.js
@@ -14,4 +14,17 @@ document.addEventListener("DOMContentLoaded", function(){
     let previousScreen = document.querySelector(".previous");
     let currentScreen = document.querySelector(".current");
 
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+
 })
+
+function handleNumber(num){
+    if(currentValue.length <= 9){
+        currentValue += num;
+    }
+    
+
+}
